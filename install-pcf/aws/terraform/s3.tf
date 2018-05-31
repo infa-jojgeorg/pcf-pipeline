f0475786add9524c1e0c2eb4bdee95a38827eeb9
jojinjg <jojinjg@gmail.com>
2018-05-31 18:16:47 +0530

    Update s3.tf

--- install-pcf/aws/terraform/s3.tf
+++ install-pcf/aws/terraform/s3.tf
@@ -1,54 +1,54 @@
-resource "aws_s3_bucket" "bosh" {
-    bucket = "${var.prefix}-bosh"
+resource "aws_s3_bucket" "bosh18" {
+    bucket = "${var.prefix}-bosh18"
     acl = "private"
     force_destroy= true
 
     tags {
-        Name = "${var.prefix}-bosh"
+        Name = "${var.prefix}-bosh18"
         Environment = "${var.prefix}"
     }
 }
 
-resource "aws_s3_bucket" "buildpacks" {
-    bucket = "${var.prefix}-buildpacks"
+resource "aws_s3_bucket" "buildpacks18" {
+    bucket = "${var.prefix}-buildpacks18"
     acl = "private"
     force_destroy= true
 
     tags {
-        Name = "${var.prefix}-buildpacks"
+        Name = "${var.prefix}-buildpacks18"
         Environment = "${var.prefix}"
     }
 }
 
-resource "aws_s3_bucket" "droplets" {
-    bucket = "${var.prefix}-droplets"
+resource "aws_s3_bucket" "droplets18" {
+    bucket = "${var.prefix}-droplets18"
     acl = "private"
     force_destroy= true
 
     tags {
-        Name = "${var.prefix}-droplets"
+        Name = "${var.prefix}-droplets18"
         Environment = "${var.prefix}"
     }
 }
 
-resource "aws_s3_bucket" "packages" {
-    bucket = "${var.prefix}-packages"
+resource "aws_s3_bucket" "packages18" {
+    bucket = "${var.prefix}-packages18"
     acl = "private"
     force_destroy= true
 
     tags {
-        Name = "${var.prefix}-packages"
+        Name = "${var.prefix}-packages18"
         Environment = "${var.prefix}"
     }
 }
 
-resource "aws_s3_bucket" "resources" {
-    bucket = "${var.prefix}-resources"
+resource "aws_s3_bucket" "resources18" {
+    bucket = "${var.prefix}-resources18"
     acl = "private"
     force_destroy= true
 
     tags {
-        Name = "${var.prefix}-resources"
+        Name = "${var.prefix}-resources18"
         Environment = "${var.prefix}"
     }
 }
